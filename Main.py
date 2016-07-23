c14f4fec1108b195e1921017746427bb5cfc4d98
benrip <ben_rippley@yahoo.com>
2016-07-23 10:07:23 -0400

    Fix initial commit

--- Main.py
+++ Main.py
@@ -1,1 +1,28 @@
-/home/ben/Documents/python/Projects/Snake/Main.py
+import os
+import sys
+import pygame
+from pygame.locals import *
+import Segment
+
+pygame.init()
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+
+segment_height = 30
+segment_width = 30
+segment_padding = 3
+
+snake = pygame.sprite.Group()
+
+screen_width = 492
+screen_height = 492
+screen = pygame.display.set_mode([screen_width, screen_height])
+
+while True:
+    spritelist.update()
+
+    spritelist.draw(screen)
+
+    pygame.display.flip()
--- Segment.py
+++
@@ -1,1 +0,0 @@
-/home/ben/Documents/python/Projects/Snake/Segment.py
