e44f1793fd0a7288be6e9debb55d5acedcb80da9
benrip <ben_rippley@yahoo.com>
2016-07-25 09:09:15 -0400

    Fixed wall collision, shows score, game loops, no longer spwans food under snake

--- Food.py
+++ Food.py
@@ -2,10 +2,26 @@ import pygame
 from pygame.locals import *
 import random
 
+x = None
+y = None
+
 class Food(pygame.sprite.Sprite):
 
-    def __init__(self, color):
+    def __init__(self, color, snake_segment):
         super().__init__()
+
+        def ranCoords():
+            global x
+            global y
+
+            x = 36 * random.randint(0, 14)
+            y = 36 * random.randint(0, 14)
+
+        def getx(snake_segment):
+            return snake_segment.rect.x
+
+        def gety(snake_segment):
+            return snake_segment.rect.y
 
         height = 33
         width = 33
@@ -15,8 +31,12 @@ class Food(pygame.sprite.Sprite):
 
         self.rect = self.image.get_rect()
 
-        x = 36 * random.randint(0, 14)
-        y = 36 * random.randint(0, 14)
+        ranCoords()
+
+        for i in range(0, len(snake_segment)):
+            if x == getx(snake_segment[i]) or y == gety(snake_segment[i]):
+                ranCoords()
+                i = 0
 
         self.rect.x = x
         self.rect.y = y
